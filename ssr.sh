e19f738dded20faf40a0880b17fddd0a78a283f5
hesoyam-oke <76074457+hesoyam-oke@users.noreply.github.com>
2021-08-04 07:56:43 +0700

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -122,7 +122,7 @@ if [[ ${OS} == "centos" ]]; then
 }
 Start_SSR(){
 	check_pid
-	wget -O /etc/init.d/ssrmu "https://raw.githubusercontent.com/sukirmanoke/project/main/ssrmu"
+	wget -O /etc/init.d/ssrmu "https://raw.githubusercontent.com/hesoyam-oke/project/main/ssrmu"
 	/etc/init.d/ssrmu start
 }
 Install_SSR(){
