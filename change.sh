4071aeb9e7f984ceb4583efa235631ef9928775a
hesoyam-oke <76074457+hesoyam-oke@users.noreply.github.com>
2021-08-08 05:06:58 +0700

    Update change.sh

--- change.sh
+++ change.sh
@@ -2,30 +2,20 @@
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- ifconfig.co);
-echo "Checking VPS"
-IZIN=$( curl http://akses.heyosam.me:81/sukirmanoke | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Only For Premium Users"
-exit 0
-fi
 clear
 echo -e ""
-echo -e "======================================"
+echo -e "======================================" | lolcat
 echo -e ""
-echo -e "     [1]  Change Port Stunnel4"
-echo -e "     [2]  Change Port OpenVPN"
-echo -e "     [3]  Change Port Wireguard"
-echo -e "     [4]  Change Port Vmess"
-echo -e "     [5]  Change Port Vless"
-echo -e "     [6]  Change Port Trojan"
-echo -e "     [7]  Change Port Squid"
-echo -e "     [8]  Change Port SSTP"
-echo -e "     [x]  Exit"
-echo -e "======================================"
+echo -e "     [1]  Change Port Stunnel4" | lolcat
+echo -e "     [2]  Change Port OpenVPN" | lolcat
+echo -e "     [3]  Change Port Wireguard" | lolcat
+echo -e "     [4]  Change Port Vmess" | lolcat
+echo -e "     [5]  Change Port Vless" | lolcat
+echo -e "     [6]  Change Port Trojan" | lolcat
+echo -e "     [7]  Change Port Squid" | lolcat
+echo -e "     [8]  Change Port SSTP" | lolcat
+echo -e "     [x]  Exit"| lolcat
+echo -e "======================================" | lolcat
 echo -e ""
 read -p "     Select From Options [1-8 or x] :  " port
 echo -e ""
